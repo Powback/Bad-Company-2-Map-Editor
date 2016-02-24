1b15992f7fa0752ca53dd505e652bf163546ce96
Powback <matsabcdefg@hotmail.com>
2016-02-24 01:04:41 +0100

    Fixed saving (for real this time)
    Frostbite does not like 1. It does like 1.0 however.
    
    So all “1” has been changed to 1.0.
    
    Frostbite, you need to chill.

--- Assets/Scripts/Inst.cs
+++ Assets/Scripts/Inst.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using BC2;
 
+
 [SelectionBase]
 public class BC2Instance : MonoBehaviour {
 	public int id;
@@ -77,17 +78,85 @@ public class BC2Instance : MonoBehaviour {
 
 
             Matrix4x4 matrix = new Matrix4x4(); 
-            matrix.SetTRS(pos,rot,Vector3.one);
+			matrix.SetTRS(pos.normalized,Normalize(rot),Vector3.one);
             Matrix4x4 m = matrix;
+			Quaternion kek;
+
+			Vector3 f = transform.forward;
+			Vector3 u = transform.up;
+			Vector3 r = transform.right * -1;
+
+			string m02 = ( m.m02 ).ToString();
+			string m10 = ( m.m10 * -1).ToString();
+			string m00 = ( m.m00 * -1).ToString();
+			string m21 = ( m.m21 * -1).ToString();
+			string m11 = ( m.m11 ).ToString();
+			string m01 = ( m.m01 ).ToString();
+			string m22 = ( m.m22 ).ToString();
+			string m12 = ( m.m12 ).ToString();
+			string m20 = ( m.m20  * -1).ToString ();
+
+			if( m.m02.ToString() == "1") { 	m02 = "1.0"; } else if(m.m02.ToString() == "-1") { m02 = "-1.0";}
+			if( m.m10.ToString() == "1") { 	m10 = "1.0"; } else if(m.m10.ToString() == "-1") { m10 = "-1.0";}  
+			if( m.m00.ToString() == "1") { 	m00 = "1.0"; } else if(m.m00.ToString() == "-1") { m00 = "-1.0";}  
+			if( m.m21.ToString() == "1") { 	m21 = "1.0"; } else if(m.m21.ToString() == "-1") { m21 = "-1.0";}  
+			if( m.m11.ToString() == "1") { 	m11 = "1.0"; } else if(m.m11.ToString() == "-1") { m11 = "-1.0";}  
+			if( m.m01.ToString() == "1") { 	m01 = "1.0"; } else if(m.m01.ToString() == "-1") { m01 = "-1.0";}  
+			if( m.m22.ToString() == "1") { 	m22 = "1.0"; } else if(m.m22.ToString() == "-1") { m22 = "-1.0";}  
+			if( m.m12.ToString() == "1") { 	m12 = "1.0"; } else if(m.m12.ToString() == "-1") { m12 = "-1.0";}  
+			if( m.m20.ToString() == "1") { 	m20 = "1.0"; } else if(m.m20.ToString() == "-1") { m20 = "-1.0";}  	
+
 
 			matrixstring =
-				Math.Round(m.m02, 9) + "/" + (Math.Round(m.m10, 9) * -1) + "/" + (Math.Round(m.m00, 9) * -1) + "/*zero*/" +
-				Math.Round(m.m21, 9) + "/" + Math.Round(m.m11, 9) + "/" + Math.Round(m.m01, 9) + "/*zero*/" +
-				Math.Round(m.m22, 9) + "/" + Math.Round(m.m12, 9) + "/" + (Math.Round(m.m20, 9) * -1) + "/*zero*/" +
+				m02+ "/" + m10 + "/" + m00 + "/*zero*/" +
+				m21 + "/" + m11+ "/" + m01+ "/*zero*/" +
+				m22+ "/" + m12+ "/" + m20+ "/*zero*/" +
 				pos.z + "/" + pos.y + "/" + pos.x + "/*zero*";
-			
 
             Util.GetComplex("Transform", instance).value = matrixstring;
-        }
+        
+		}
     }
+
+	public Quaternion Normalize(Quaternion q){
+		double nqx = double.Parse (q.x.ToString ());
+		double nqy = double.Parse (q.y.ToString ());
+		double nqz = double.Parse (q.z.ToString ());
+		double nqw = double.Parse (q.w.ToString ());
+
+		double norm2 = nqx*nqx + nqy*nqy + nqz*nqz + nqw*nqw;
+		if (norm2 > Double.MaxValue) 
+		{
+			// Handle overflow in computation of norm2
+			double rmax = 1.0/Max(Math.Abs(nqx),
+				Math.Abs(nqy), 
+				Math.Abs(nqz),
+				Math.Abs(nqw)); 
+
+			nqx *= rmax;
+			nqy *= rmax; 
+			nqz *= rmax;
+			nqw *= rmax;
+			norm2 = nqx*nqx + nqy*nqy + nqz*nqz + nqw*nqw;
+		} 
+		double normInverse = 1.0 / Math.Sqrt(norm2);
+		nqx *= normInverse; 
+		nqy *= normInverse; 
+		nqz *= normInverse;
+		nqw *= normInverse; 
+
+		Quaternion quat = new Quaternion (float.Parse (nqx.ToString()), float.Parse (nqy.ToString()), float.Parse (nqz.ToString()), float.Parse (nqw.ToString()));
+		return quat;
+	}
+
+	static private double Max(double a, double b, double c, double d)
+	{ 
+		if (b > a) 
+			a = b;
+		if (c > a) 
+			a = c;
+		if (d > a)
+			a = d;
+		return a; 
+	}
 }
