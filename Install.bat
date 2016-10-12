44703d946a24afee26e6ef5f6e5cb5e19897b391
Mats Bakken <matsabcdefg@hotmail.com>
2016-10-12 17:09:48 +0200

    Revert "Added installer"
    This reverts commit eef6d35c4bd1b7a08cf5b2a80fc0452f4f22f392.

--- Install.bat
+++
@@ -1,4 +0,0 @@
-set "curpath=%cd%"
-python "Extractor\Python Extractor\archive.py" "C:\Program Files (x86)\Origin Games\Battlefield Bad Company 2"
-python "Extractor\Python Extractor\dbx.py" "%curpath%/Resources FbRB/"
-move "Resources FbRB" "Resources"
